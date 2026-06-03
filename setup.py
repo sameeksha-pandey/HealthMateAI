318b1c5303180223e792c36b701365ea5e6c8c90
Sameeksha Pandey <sameekshapandey@Sameekshas-MacBook-Air.local>
2026-06-03 17:20:12 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "HeathMateAI",
+    version = "0.1.0",
+    author ="sameeksha Pandey",
+    author_email = "sameeksha116@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
