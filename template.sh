cc9837b165a77195f003b07e8e311df1b77b1b38
Sameeksha Pandey <sameekshapandey@Sameekshas-MacBook-Air.local>
2026-06-03 14:38:58 +0530

    Folder Structure Added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directorys
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__init__.py
+touch src/helpher.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Project structure created successfully!"
